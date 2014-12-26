436d9433b6ed8a10b465a37cbce810d91e5f11c0
David García-Navas López de Cuéllar <davidgarcianavas@gmail.com>
2014-12-26 19:01:30 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,3 @@
+register_asset "javascripts/discourse/templates/discovery/categories.js.handlebars"
+register_asset "javascripts/discourse/templates/list/topic_list_item.js.handlebars"
+register_asset "javascripts/newrelic_style.js"
